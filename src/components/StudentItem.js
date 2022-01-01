4f4bd11b5f1c3cc9e7cb8e73d538352f54f2e170
gizem28 <gizemodun28@gmail.com>
2022-01-01 19:34:26 +0100

    m

--- src/components/StudentItem.js
+++ src/components/StudentItem.js
@@ -8,11 +8,18 @@ const{changeColor}=useContext(StudentContext);
 const {id, name, age, color, email}=student;
 //changeColor arrow ile cagirmamiz zorunlu yoksa otomatik cagirilir her defasinda
     return (
-        <div>
-            <h3>{name}</h3>
-            <h4>{email}</h4>
-            <h4>{age}</h4>
-            Color: <input type="text" value={color} 
+        <div style={{
+            background: student.color,
+            paddingBottom: "2rem",
+            padding: "1rem",
+            marginLeft:"8rem",
+            marginRight:"8rem",
+            marginBottom:"2rem",
+          }}>
+            <h3> Name: {name}</h3>
+            <h4>Email: {email}</h4>
+            <h4>Age: {age}</h4>
+            <h4>Color:</h4> <input type="text" value={color} 
             onChange={(e)=> changeColor(e.target.value, id)}/>
         </div>
     )
